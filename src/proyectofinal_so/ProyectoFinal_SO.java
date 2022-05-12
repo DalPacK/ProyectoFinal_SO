42c1c49b3baa631de9874d9cc8a2c1c181194780
picar <picar@DESKTOP-GPP2383>
2022-05-12 17:06:17 -0500

    Prueba

---
+++ src/proyectofinal_so/ProyectoFinal_SO.java
@@ -0,0 +1,19 @@
+/*
+
+ */
+package proyectofinal_so;
+
+/**
+ *
+ * @author picar
+ */
+public class ProyectoFinal_SO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
