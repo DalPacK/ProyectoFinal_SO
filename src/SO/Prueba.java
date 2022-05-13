4d0ecec427484933916987ca8286f22470a218a7
IDPatI <95386020+IDPatI@users.noreply.github.com>
2022-05-12 19:38:44 -0500

    asdf
    aprueba

--- src/SO/Prueba.java
+++ src/SO/Prueba.java
@@ -13,7 +13,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int Prueba =2;
     }
     
 }
