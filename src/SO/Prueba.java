050d6155537b8d83ada190b77dce172d83723156
picar <picar@DESKTOP-GPP2383>
2022-05-12 19:24:22 -0500

    Prueba de subida

---
+++ src/SO/Prueba.java
@@ -0,0 +1,19 @@
+/*
+
+ */
+package SO;
+
+/**
+ *
+ * @author picar
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
